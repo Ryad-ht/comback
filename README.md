c0bf0f31f6d910207beaa0d530f59229b472c257
Ryad <125749863+Ryad-ht@users.noreply.github.com>
2024-08-28 17:10:58 +0200

    commit and finised

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# comback
-didnt work for while so that will be my back in dev
+##Objectif de cette story :
+
+démontrer la relation entre l'ancienneté d'une entreprise dans le classement Inc. 5000 et sa croissance moyenne.
+e ndautre terme que les entreprises plus jeunes ont tendance à avoir une croissance moyenne plus élevée que les entreprises plus anciennes
+
+
+
+data source tableau public : https://public.tableau.com/app/learn/sample-data
--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-import pandas as panda
-import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+
+# Lire le fichier CSV dans un DataFrame
+df = pd.read_csv('Data Set- Inc5000 Company List_2014.csv')
+
+# Calculer la moyenne de la croissance pour chaque valeur de 'yrs_on_list'
+avg_growth_by_yrs = df.groupby('yrs_on_list')['growth'].mean()
+
+# Créer un graphique pour visualiser la relation
+plt.figure(figsize=(10, 6))
+avg_growth_by_yrs.plot(kind='bar', rot=0)
+plt.title('Impact de l\'ancienneté sur la croissance moyenne')
+plt.xlabel('Nombre d\'années dans la liste')
+plt.ylabel('Croissance moyenne')
+plt.show()
